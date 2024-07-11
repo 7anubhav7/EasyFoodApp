143427c5e13f833cf841c72d8cc9f5afe8415fa7
anubhav <122981577+anoob-leh@users.noreply.github.com>
2024-07-11 22:35:11 +0530

    added youtube link icon, progress bar, added fade effect while scrolling, some fixes

--- app/src/main/java/com/example/easyfood/videoModel/MealViewModel.kt
+++ app/src/main/java/com/example/easyfood/videoModel/MealViewModel.kt
@@ -6,47 +6,48 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.easyfood.db.MealDatabase
 import com.example.easyfood.pojo.Meal
 import com.example.easyfood.pojo.MealList
 import com.example.easyfood.retrofit.RetrofitInstance
+import kotlinx.coroutines.launch
 import okhttp3.Response
 
-class MealViewModel() : ViewModel() {
+class MealViewModel(
+    val mealDatabase: MealDatabase
+) : ViewModel() {
 
     private var mealDetailsLiveData = MutableLiveData<Meal>()
 
     fun getMealDetail(id: String) {
         RetrofitInstance.api.getMealDetails(id).enqueue(object : retrofit2.Callback<MealList> {
+
             override fun onResponse(
                 call: retrofit2.Call<MealList>,
                 response: retrofit2.Response<MealList>
             ) {
                 if (response.body() != null) {
                     mealDetailsLiveData.value = response.body()!!.meals[0]
-                } else {
+                } else
                     return
-                }
             }
+
 
             override fun onFailure(call: retrofit2.Call<MealList>, t: Throwable) {
                 Log.d("MealActivity", t.message.toString())
             }
-
-
         })
-
-
     }
 
     fun observeMealDetailsLiveData():LiveData<Meal>{
         return mealDetailsLiveData
     }
 
-
-
+    fun insertMeal(meal: Meal){
+        viewModelScope.launch {
+            mealDatabase.mealDao().delete(meal)
+        }
+    }
 
 }
-
-
-
-
